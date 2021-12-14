58e8e38d527b4ff12b74642f1468016642b60819
AaronMRobinson <amrobinson4@ualr.edu>
2021-12-14 13:38:47 +0000

    Dragon Revised and Website Update

---
+++ dragon-revised/assets/js/local-calls.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    
+    $('#slides .container').slick({
+        fade:true,
+        autoplay:true,
+        speed: 1000,
+        autoplaySpeed: 3000
+    });
+
+});
